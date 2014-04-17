7634076e409b2b102d315b36145c69c228b0ac55
Tomasz Wyszomirski <tomasz@wyszomirski.co.uk>
2014-04-17 11:40:11 +0100

    Initial Commit

---
+++ DynamicProperties/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicProperties
+//
+//  Created by Wyszo on 17/04/14.
+//  Copyright (c) 2014 Synappse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicProperties/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DynamicProperties
+//
+//  Created by Wyszo on 17/04/14.
+//  Copyright (c) 2014 Synappse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
